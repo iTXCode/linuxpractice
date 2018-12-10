275f3d8b9161202dfacec617a4bc91817d698af7
centos-7 <wangshixian@localhost.localdomain>
2018-12-10 08:32:04 -0800

    the second

--- test.c
+++ test.c
@@ -1,31 +1,33 @@
+
 #include<stdio.h>
 
 int main()
 {
-int i=0;
-int j=0;
-int a[10]={0};
-int tmp=0;
+   int i=0;
+   int j=0;
+   int a[10]={0};
+   int tmp=0;
 printf("pelase enter 10 number:");
 for(i=0;i<10;i++)
 {
-scanf("%d",a[i]);
+   scanf("%d",&a[i]);
 }
 for(i=0;i<9;i++)
 {
-for(j=0;j<9-i;j++)
-{
-if(a[j]>a[j+1])
-{
-tmp=a[j];
-a[j]=a[j+1];
-a[j+1]=tmp;
-}
-}
+  for(j=0;j<9-i;j++)
+ { 
+   if(a[j]>a[j+1])
+   {
+       tmp=a[j];
+       a[j]=a[j+1];
+       a[j+1]=tmp;
+    }
+  }
 }
 for(i=0;i<10;i++)
 {
-printf("%d ",a[i]);
+   printf("%d ",a[i]);
 }
+printf("\n");
 return 0;
 }
