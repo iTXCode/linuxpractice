b3cb160f82c54894922450054403f3e6e438b228
centos-7 <wangshixian@localhost.localdomain>
2018-12-10 08:12:12 -0800

    the second

--- test.c
+++ test.c
@@ -2,7 +2,30 @@
 
 int main()
 {
-printf("the  first\n");
-printf("hello git!\n");
+int i=0;
+int j=0;
+int a[10]={0};
+int tmp=0;
+printf("pelase enter 10 number:");
+for(i=0;i<10;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<9;i++)
+{
+for(j=0;j<9-i;j++)
+{
+if(a[j]>a[j+1])
+{
+tmp=a[j];
+a[j]=a[j+1];
+a[j+1]=tmp;
+}
+}
+}
+for(i=0;i<10;i++)
+{
+printf("%d ",a[i]);
+}
 return 0;
 }
