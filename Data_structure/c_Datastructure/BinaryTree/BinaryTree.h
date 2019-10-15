e29bf970d5f807a119e280496682beede46b0442
jack-wang128801 <1632672310@qq.com>
2019-10-15 22:35:20 +0800

    add some function

--- Data_structure/c_Datastructure/BinaryTree/BinaryTree.h
+++ Data_structure/c_Datastructure/BinaryTree/BinaryTree.h
@@ -9,6 +9,9 @@ typedef struct TreeNode{
   struct TreeNode* _right;
 }TreeNode,*Bitree;
 
+
+
+//按照先序遍历的方式建立二叉树
 void CreateTree(Bitree* root){
   TNType node;
   scanf("%c",&node);
@@ -77,6 +80,7 @@ int PostOrder(Bitree root){
 }
 
 
+
 //采用分治算法
 int Leaf(Bitree root){
   int leaf=0;
@@ -89,5 +93,58 @@ int Leaf(Bitree root){
     leaf=Leaf(root->_left)+Leaf(root->_right);
   }
   return leaf;
+}
+
+
+//求二叉树的高度/深度
+int TreeDepth(Bitree root){
+  if(root==NULL){
+    return 0;
+  }
+  int left=TreeDepth(root->_left);
+  int right=TreeDepth(root->_right);
+  return (left>right?left:right)+1;
+}
+
+//求第K层的结点个数
+
+int KNode(Bitree root,int k){
+  if(root==NULL){
+    return 0;
+  }
+  if(k==1)
+    return 1;
+  return KNode(root->_left,k-1)+KNode(root->_right,k-1);
+}
+
+
+//在二叉树结点中查找某个值
+//算法思路:
+//1.先确定根结点是否存在
+//2.若根存在的话再确定根的值是否和所要的值是否相等
+//3.若相等的话再去根节点的左子树中查找指定的值
+//4.再到右子树中查找指定的值
+//若上述过程中找到了对应的值就返回对应的结点，
+//若没有找到的话就返回空值
+TreeNode*  Tree_Value_Find(Bitree root,char value){
+  if(root==NULL){
+    return NULL;
+  }
+  //在根结点找到了要找的值
+  if(root->_value==value){
+    return root;
+  }
+  //当在根结点没有找到对应的值的时候
+  //到该根结点对应的左右子树中去找
+  TreeNode* left_result=Tree_Value_Find(root->_left,value);
+  if(left_result!=NULL){
+    return left_result;
+  }
+  TreeNode* right_result=Tree_Value_Find(root->_right,value);
+  if(right_result!=NULL){
+    return right_result;
+  }else{
+    return NULL;
+  }
 }
 
--- Data_structure/c_Datastructure/BinaryTree/tree.c
+++ Data_structure/c_Datastructure/BinaryTree/tree.c
@@ -17,6 +17,23 @@ void TestTree(){
   printf("\n");
   printf("叶子结点个数:%d\n",PostOrder(tree));
   printf("叶子结点个数:%d\n",Leaf(tree));
+
+  //求二叉树的深度
+  int tree_depth=TreeDepth(tree);
+  printf("该树的深度为:%d\n",tree_depth);
+  int k=0;
+  printf("请输入要求的层数:");
+  scanf("%d",&k);
+  int k_node_num=KNode( tree,k);
+  printf("第K层的结点个数为:%d\n",k_node_num);
+  //确定二叉树中是否有指定的值存在‘
+
+  TreeNode* find_result=Tree_Value_Find(tree,'D');
+  if(find_result==NULL){
+    printf("在该二叉树中没有找到指定的值,它的地址为:%p\n",find_result);
+  }else{
+    printf("在该二叉树中找到了该值:%c 该结点地址为:%p\n",find_result->_value,find_result);
+  }
 }                             
               
 int main(){
