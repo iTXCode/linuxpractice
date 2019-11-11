e492a519c3b1181e3e9cee3a4500e82d90794040
jack-wang128801 <1632672310@qq.com>
2019-11-11 23:47:53 +0800

    max heap

---
+++ Data_structure/cpp_Datastructure/Max_Heap/Heap.hpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<malloc.h>
+#include<memory.h>
+
+template <class T>
+
+class Heap{
+  public:
+    Heap(){
+      _array=NULL;
+    _size=0;
+    }
+
+    void Swap(T* a,T* b){
+      T tmp=*a;
+      *a=*b;
+      *b=tmp;
+    }
+    
+
+    void PrintHeap(const Heap<T>* heap){
+      for(int i=0;i<heap->_size;i++){
+        printf("%d ",heap->_array[i]);
+      }
+      printf("\n");
+    }
+
+
+    void AdjustDown(T* array,int size,int root){
+     
+      while(1){
+        int left=2*root+1;
+        int right=2*root+2;
+
+        if(left>=size){
+          return;
+        }
+
+        int max=left;
+        if(right<size&&array[right]>array[left]){
+          max=right;
+        }
+
+        if(array[root]>=array[max]){
+          return;
+        }
+
+        Swap(array+root,array+max);
+        root=max; 
+      }
+
+    }
+
+
+    void CreateHeap(T* array,int size){
+      for(int i=(size-2)/2;i>=0;i--){
+        AdjustDown(array,size,i);
+      }
+    }
+
+
+
+    void HeapInit(Heap <T>* heap,const T* array,int size){
+      heap->_array=(T*)malloc(sizeof(T)*size);
+      memcpy(heap->_array,array,sizeof(T)*size);
+      heap->_size=size;
+      CreateHeap(heap->_array,heap->_size);
+    }
+
+
+    void AdjustUp( T* array,int size,int child){
+      while(child!=0){
+        if(child>=size){
+          return;
+        }
+
+        int root=(child-1)/2;
+        if(array[root]>=array[child]){
+          return; 
+        }
+
+        Swap(array+root,array+child);
+        child=root;
+      }
+    }
+
+    void HeapPush(Heap<T>* heap,T value){
+      heap->_array[heap->_size++]=value;
+      AdjustUp(heap->_array,heap->_size,heap->_size-1);
+    }
+    
+
+
+    void HeapPop(Heap<T>* heap){
+      Swap(heap->_array,heap->_array+(heap->_size-1));
+      heap->_size--;
+      AdjustDown(heap->_array,heap->_size,0);
+    }
+
+    bool  HeapFind(const Heap<T>* heap,T  value){
+      for(int i=0;i<heap->_size;i++){
+        if(value==heap->_array[i]){
+          return true;
+        }
+      }
+      return false;
+    }
+
+
+    void HeapModify(Heap<T>* heap,T value,T M_value){
+      int index=0;
+      for(int i=0;i<heap->_size;i++){
+        if(value==heap->_array[i]){
+          index=i;
+        }
+      }
+
+      heap->_array[index]=M_value;
+    }
+
+
+  private:
+    T* _array;
+    int _size;
+};
+
---
+++ Data_structure/cpp_Datastructure/Max_Heap/heap.cc
@@ -0,0 +1,39 @@
+#include"Heap.hpp"
+
+void  TestHeap(){
+  int array[]={91,3,84,56,56,78,34,65,86,24};
+  int size=sizeof(array)/sizeof(array[0]);
+  Heap<int> heap;
+  heap.HeapInit(&heap,array,size);
+  heap.PrintHeap(&heap);
+  heap.HeapPop(&heap);
+  heap.PrintHeap(&heap);
+  printf("请输入您要插入的值:");
+  int value=0;
+  std::cin>>value;
+  heap.HeapPush(&heap,value);
+  heap.PrintHeap(&heap);
+  std::cout<<"请输入您要查找的值:";
+  std::cin>>value;
+  int ret=heap.HeapFind(&heap,value);
+  if(ret==1){
+    std::cout<<"你要找的值在堆序列中!"<<std::endl;
+  }else{
+    std::cout<<"您要找的值不在堆序列中!"<<std::endl;
+  }
+  
+  int m_value;
+  printf("请输入您要修改的值:");
+  std::cin>>value;
+  printf("请输入您修改后的值:");
+  std::cin>>m_value;
+  heap.HeapModify(&heap,value,m_value);
+  heap.PrintHeap(&heap);
+
+}
+
+
+int main(){
+  TestHeap();
+  return 0;
+}
